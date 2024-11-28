37b397c42f60da0f4d6b4a59305061783e3b9adf
Никита <jelly0050@gmail.com>
2024-11-28 22:29:37 +0300

    Бонусное задание 28.11

--- modul_2_bonus.py
+++ modul_2_bonus.py
@@ -6,4 +6,4 @@ for sum in range(2, number + 1):
     if number % sum == 0:
         for numbers in range(1, (sum + 1) //2):
             a = sum - numbers
-            print(numbers, a)
+            print(numbers, a, end = '; ')
