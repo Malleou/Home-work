60644d12e6a237887f8a1f95acb59e70311a9d79
Никита <jelly0050@gmail.com>
2024-11-28 22:24:27 +0300

    Бонусное задание 28.11

---
+++ modul_2_bonus.py
@@ -0,0 +1,9 @@
+number = int(input('Число из первой части: '))
+sum_counter = 0
+
+for sum in range(2, number + 1):
+    sum_counter = 0
+    if number % sum == 0:
+        for numbers in range(1, (sum + 1) //2):
+            a = sum - numbers
+            print(numbers, a)
