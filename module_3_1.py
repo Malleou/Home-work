147865de354b6eb2f16e0e11ecac2f6d7aff0297
Никита <jelly0050@gmail.com>
2024-11-30 21:47:55 +0300

    30.11

---
+++ module_3_1.py
@@ -0,0 +1,34 @@
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+
+def string_info(string):
+    str_ = (len(string), string.upper(), string.lower())
+    count_calls()
+    return str_
+
+
+
+def is_contains(string, list_to_search):
+    string = string.lower()
+    boolean = False
+
+    for text in list_to_search:
+        #boolean = False
+        text = text.lower()
+        if string == text:
+            boolean = True
+            #print(boolean)
+    count_calls()
+    return boolean
+
+
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
