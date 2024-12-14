529c4e7af42cb643f01931062ec49d21d3574c27
Никита <jelly0050@gmail.com>
2024-12-14 18:48:26 +0300

    3/3 часть задачи module_4_1.py

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import module_4_true_math as true_divider
+import module_4_fake_math as fake_divider
+
+result1 = fake_divider.divide(69, 3)
+result2 = fake_divider.divide(3, 0)
+result3 = true_divider.divide(49, 7)
+result4 = true_divider.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
